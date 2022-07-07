85186d2ca0288c9f0d1150ec5c71b93ebbb889c5
Dale Fugier <dale@mcneel.com>
2022-07-07 10:00:32 -0700

    Initial commit

---
+++ ImportSeligPlugin.cs
@@ -0,0 +1,102 @@
+﻿using Rhino;
+using Rhino.FileIO;
+using Rhino.Geometry;
+using Rhino.PlugIns;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ImportSelig
+{
+  public class ImportSeligPlugin : FileImportPlugIn
+  {
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public ImportSeligPlugin()
+    {
+      Instance = this;
+    }
+
+    /// <summary>
+    /// Gets the only instance of this plug-in.
+    /// </summary>
+    public static ImportSeligPlugin Instance { get; private set; }
+
+    /// <summary>
+    /// Defines file extensions that this import plug-in is designed to read.
+    /// </summary>
+    /// <param name="options">Options that specify how to read files.</param>
+    /// <returns>A list of file types that can be imported.</returns>
+    protected override FileTypeList AddFileTypes(FileReadOptions options)
+    {
+      var fileTypeList = new Rhino.PlugIns.FileTypeList();
+      fileTypeList.AddFileType("Selig Airfoil Data (*.dat)", "dat");
+      return fileTypeList;
+    }
+
+    /// <summary>
+    /// Called when a user requests to import a .dat file.
+    /// </summary>
+    /// <param name="filename">The complete path to the new file.</param>
+    /// <param name="index">The index of the file type as it had been specified by the AddFileTypes method.</param>
+    /// <param name="doc">The document to be written.</param>
+    /// <param name="options">Options that specify how to write file.</param>
+    /// <returns>A value that defines success or a specific failure.</returns>
+    protected override bool ReadFile(string filename, int index, RhinoDoc doc, FileReadOptions options)
+    {
+      bool rc = false;
+      try
+      {
+        var lines = File.ReadAllLines(filename);
+        if (null != lines && lines.Length > 2)
+        {
+          var name = lines[0].Trim();
+          var points = new List<Point3d>();
+          var delimiters = new char[] { ' ', '\t' };
+
+          for (var i = 1; i <lines.Length; i++)
+          {
+            var line = lines[i].Trim();
+            if (string.IsNullOrEmpty(line))
+              continue;
+
+            var elements = line.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            if (null == elements || 2 != elements.Length)
+              continue;
+
+            if (double.TryParse(elements[0], out var x) && double.TryParse(elements[1], out var z))
+            {
+              var point = new Point3d(x, 0.0, z);
+              points.Add(point);
+            }
+          }
+
+          if (points.Count > 2)
+          {
+            var curve = Curve.CreateInterpolatedCurve(points, 3);
+            if (null != curve)
+            {
+              var attributes = doc.CreateDefaultAttributes();
+              attributes.Name = name;
+              var id = doc.Objects.AddCurve(curve, attributes);
+              if (id != Guid.Empty)
+                rc = true;
+            }
+          }
+        }
+      }
+      catch (Exception)
+      {
+        // TODO...
+      }
+
+      if (!rc)
+        RhinoApp.WriteLine("Error reading Selig Airfoil Data.");
+
+      doc.Views.Redraw();
+
+      return rc;
+    }
+  }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using Rhino.PlugIns;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Plug-in Description Attributes - all of these are optional.
+// These will show in Rhino's option dialog, in the tab Plug-ins.
+[assembly: PlugInDescription(DescriptionType.Address, "146 North Canal St, Suite 320\r\nSeattle, WA 98103")]
+[assembly: PlugInDescription(DescriptionType.Country, "United States")]
+[assembly: PlugInDescription(DescriptionType.Email, "dale@mcneel.com")]
+[assembly: PlugInDescription(DescriptionType.Phone, "(206) 545-7000")]
+[assembly: PlugInDescription(DescriptionType.Fax, "(206) 545-7321")]
+[assembly: PlugInDescription(DescriptionType.Organization, "Robert McNeel & Associates")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
+
+// Icons should be Windows .ico files and contain 32-bit images in the following sizes: 16, 24, 32, 48, and 256.
+[assembly: PlugInDescription(DescriptionType.Icon, "ImportSelig.EmbeddedResources.plugin-import.ico")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+// This will also be the Guid of the Rhino plug-in
+[assembly: Guid("20569192-4214-48B2-9E88-39A2D7B4B45A")]
